1b17574e0d6b82f8838ccfbf616847490dcc24e9
patriicke <patrickndayambaje4@gmail.com>
2024-06-24 13:36:47 +0200

    chore: loader added successfully

--- mobile/src/components/elements/loader/Loader.tsx
+++ mobile/src/components/elements/loader/Loader.tsx
@@ -1,53 +1,12 @@
-import React, { useEffect, useRef } from "react";
-import { StyleSheet, Animated, Easing } from "react-native";
+import React from "react";
+import { ActivityIndicator } from "react-native";
 
 type LoaderProps = {
   className?: string;
 };
 
 const Loader = (props: LoaderProps) => {
-  const rotateAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    const startRotation = () => {
-      rotateAnim.setValue(0);
-      Animated.loop(
-        Animated.timing(rotateAnim, {
-          toValue: 1,
-          duration: 2000,
-          easing: Easing.linear,
-          useNativeDriver: true
-        })
-      ).start();
-    };
-
-    startRotation();
-  }, [rotateAnim]);
-
-  const rotateInterpolate = rotateAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: ["0deg", "360deg"]
-  });
-
-  const rotatingStyle = {
-    transform: [{ rotate: rotateInterpolate }]
-  };
-
-  return (
-    <Animated.View
-      style={[styles.loader, rotatingStyle]}
-      className={`h-6 w-6 ${props.className}`}
-    />
-  );
+  return <ActivityIndicator size='small' color='#0000ff' />;
 };
-
-const styles = StyleSheet.create({
-  loader: {
-    borderWidth: 10,
-    borderColor: "#fff",
-    borderTopColor: "transparent",
-    borderRadius: 50
-  }
-});
 
 export default Loader;
